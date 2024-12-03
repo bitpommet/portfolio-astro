ac3b3b530920478371fa71dec95f67a4055f26cd
bitpommet <188138130 +bitpommet@users.noreply.github.com>
2024-12-03 14:32:04 +0900

    revised

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://portfolio-astro-six-wine.vercel.app/',
+  site: "https://portfolio-astro-six-wine.vercel.app/",
   integrations: [icon()],
 });
