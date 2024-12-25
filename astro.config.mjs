e7a27104ade724333398795870f5ba2ec301aa89
bitpommet <188138130 +bitpommet@users.noreply.github.com>
2024-12-25 15:26:01 +0900

    revised

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://portfolio-astro-six-wine.vercel.app/",
-  integrations: [icon()],
+  integrations: [icon(), mdx(),],
 });
